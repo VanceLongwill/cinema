4c040c6640ff8098145d4524a2c57aa09d904a1f
Vance Longwill <vancelongwill@gmail.com>
2019-08-07 17:46:41 +0300

    Updates

---
+++ precache-manifest.0147fefad7866dce65f9565d161c2dfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da53a405b4e84825f12c",
+    "url": "/cinema/static/css/main.e06b4ec9.chunk.css"
+  },
+  {
+    "revision": "da53a405b4e84825f12c",
+    "url": "/cinema/static/js/main.826b2e10.chunk.js"
+  },
+  {
+    "revision": "99b60e0412e8a1fd4cbd",
+    "url": "/cinema/static/js/runtime~main.3f5cd292.js"
+  },
+  {
+    "revision": "ab555fbd4c9af33d1cfc",
+    "url": "/cinema/static/js/2.95034726.chunk.js"
+  },
+  {
+    "revision": "3cea6b16356c93caf7e977d1b4db401a",
+    "url": "/cinema/index.html"
+  }
+];
